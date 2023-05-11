a94722e18fd59327061696b23aa8144f2e19ca74
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-05-11 15:16:54 +0530

    Type aliases.. signle place to define types

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-let employee: {
+// Type aliases
+type Employee = {
   readonly id: number;
   name: string;
   retire: (date: Date) => void;
-} = {
+};
+
+let employee: Employee = {
   id: 1,
   name: "Shrikant",
   retire: (date: Date) => {
     console.log(date);
   },
 };
-
-// Make sure the code we write is conceptually correct. We shouldn't blindly use features of TS.
-// e.g. every employee should have a name.
