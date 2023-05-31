db1bc42430c9a68738f2816f57bf10fd7a5fc2fb
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-05-31 16:34:39 +0530

    Parameter Properties

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 // Access modifiers: public, private, protected
 
 class Account {
-  readonly id: number;
-  owner: string;
-  private _balance: number;
   nickname?: string;
 
-  constructor(id: number, owner: string, balance: number) {
-    this.id = id;
-    this.owner = owner;
-    this._balance = balance;
-  }
+  constructor(
+    public readonly id: number,
+    public owner: string,
+    private _balance: number
+  ) {}
 
   deposit(amount: number) {
     if (amount <= 0) throw new Error("Invalid amt or Cant deposit");
